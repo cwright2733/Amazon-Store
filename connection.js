d2221ea059ec5b1d17a18f06de41694c21ce9968
cameron wright <arabia.cw@gmail.com>
2019-05-21 19:37:33 -0400

    connection

--- connection.js
+++ connection.js
@@ -18,7 +18,7 @@ var inquirer = require("inquirer");
 
   connection.connect(function (err) {
     if (err) throw err;
-    //console.log("connected as id " + connection.threadId + "\n");
+    console.log("connected as id " + connection.threadId + "\n");
 
   });
   
